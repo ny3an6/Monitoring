477d6fb4221ad31b0af2b721ec6e9d6eb99621f4
ny3an6 <ndmitrenko38@gmail.com>
2021-06-05 16:24:44 +0300

    view main info is ready

---
+++ src/components/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-list-group>
+      <b-list-group-item label="Spinning">MCC: {{this.mainInfo.mcc}}</b-list-group-item>
+      <b-list-group-item label="Spinning">MNC: {{this.mainInfo.mnc}}</b-list-group-item>
+      <b-list-group-item label="Spinning">LAC: {{this.mainInfo.lac}}</b-list-group-item>
+      <b-list-group-item label="Spinning">Cell Id: {{this.mainInfo.cellId}}</b-list-group-item>
+      <b-list-group-item label="Spinning">Номер Радиоканала: {{this.mainInfo.ch}}</b-list-group-item>
+      <b-list-group-item label="Spinning">Уровень сигнала: {{this.mainInfo.rssi}}</b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  name: "Main",
+  data() {
+    return {
+      mainInfo: {},
+    };
+  },
+  
+  async created(){
+    console.log("Hello")
+    //setTimeout(() => { console.log("мир"); }, 3000);
+    await Axios.get("http://localhost:8888")
+      .then(response => {
+        console.log(response.data);
+        this.mainInfo = response.data;       
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
